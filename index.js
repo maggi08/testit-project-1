1e6725f1040106a99ce2af41ed51fad3de29c955
Mako <maggi08mus@gmail.com>
2023-09-07 21:05:36 +0600

    object keys values and entries

--- index.js
+++
@@ -1,5 +0,0 @@
-console.log('Our app initialized');
-
-console.log('second commit');
-console.log('hello world ');
-console.log('hello world ');
---
+++ js/test.js
@@ -0,0 +1,84 @@
+// let array = ['one', 'two', 'three']
+// for (let index = 0; index < array.length; index++) {
+//   // console.log(array[index]);
+// }
+
+
+// let user = {
+//   name: 'Magzhan',
+//   surname: 'Mussaiyn'
+// }
+
+// // for (let i in user) {
+// //   console.log(user[i]);
+// // }
+
+// console.log(user);
+
+// console.log(Object.keys(user));
+// // Object.keys(user).forEach(e => console.log(user[e]))
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+// Object.entries(user).forEach(e => {
+//   console.log(`key - ${e[0]}, value - ${e[1]}`);
+// })
+
+// let person = {
+//   name: 'Jhon',
+//   last_name: 'Doe',
+//   age: 14
+// }
+
+// function doubleAge(age) {
+//   return age * 2
+// }
+
+// let { age, name, last_name: surename } = person
+
+// console.log(person.age);
+// console.log(age, name, surename);
+// console.log(doubleAge(person.age))
+// console.log(doubleAge(age))
+
+
+// let numbers = [45, 62, 73, 1, 0, 19, 10]
+
+// let [first, second, ...others] = numbers
+
+// console.log('numbers ', numbers);
+// console.log('first ', first);
+// console.log('second ', second);
+// console.log('others ', others);
+
+
+// let car = {
+//   model: 'mers',
+//   wheels: 4,
+// }
+
+// for (let [key, value] of Object.entries(car)) {
+//   console.log(`${key}-${value}`);
+// }
+
+// let words = {}
+// [words.first, words.second] = 'Hello world'.split(' ') // ['Hello', 'world']
+// console.log(string.split(' '));
+// console.log(words);
+
+
+// let name = 'Jhon'
+// let surename = 'Doe'
+// console.log(name, surename);
+// [name, surename] = [surename, name]
+// console.log(name, surename);
+
+
+// let date = new Date()
+// console.log(date);
+
+// let user = {
+//   "name": "Jhon",
+//   "surename": "Doe"
+// }
+// console.log(user);
