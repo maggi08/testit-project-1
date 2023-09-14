45e5cbfedea804cd22964e2283d32a0d8860338e
Mako <maggi08mus@gmail.com>
2023-09-14 22:32:59 +0600

    callbacks

--- js/test.js
+++ js/test.js
@@ -82,3 +82,95 @@
 //   "surename": "Doe"
 // }
 // console.log(user);
+
+
+// try {
+//   // console.log('Hello');
+//   console.log('start');
+//   console.log(hello);
+//   console.log('end');
+// } catch (err) {
+//   console.log('Has Error name', err.name);
+//   console.log('Has Error message', err.message);
+//   console.log('Has Error stack', err.stack);
+//   console.log('Has Error err', err);
+// }
+
+
+// неработает с SyntaxError
+// try {
+//   --{{ { } }
+// }
+// } catch (err) {
+//   console.log('Has Error name', err.name);
+//   console.log('Has Error message', err.message);
+//   console.log('Has Error stack', err.stack);
+//   console.log('Has Error err', err);
+
+// }
+
+
+
+// let json = "{ некорректный JSON }";
+// let json = '{ "name":"Jhon" }';
+
+// try {
+
+//   let user = JSON.parse(json); // <-- тут возникает ошибка...
+//   alert(user.name); // не сработает
+
+// } catch (e) {
+//   // ...выполнение прыгает сюда
+//   alert("Извините, в данных ошибка, мы попробуем получить их ещё раз.");
+//   alert(e.name);
+//   alert(e.message);
+// }
+
+
+// try {
+//   const burgerBtn = document.getElementById('burgerBtn3')
+//   console.log(burgerBtn.innerHTML);
+// } catch (err) {
+//   console.log(err);
+// }
+
+
+// try {
+//   const burgerBtn = document.getElementById('burgerBtn3')
+//   // if (!burgerBtn) throw new Error('burgerBtn3 is not in DOM')
+//   // if (!burgerBtn) throw new ReferenceError('burgerBtn3 is not in DOM')
+//   // if (!burgerBtn) throw new SyntaxError('burgerBtn3 is not in DOM')
+//   console.log(burgerBtn.innerHTML);
+// } catch (err) {
+//   console.log(err);
+// }
+
+// function hello() {
+//   console.log('hello');
+// }
+// try {
+//   console.log('start');
+//   blalblalabl()
+//   console.log('end');
+// } catch (err) {
+//   console.log(err);
+// } finally {
+//   hello()
+// }
+
+// try {
+//   console.log('start 2');
+//   console.log('end 2');
+// } catch (err) {
+//   console.log('err 2', err);
+// } finally {
+//   hello()
+// }
+
+// console.log(window);
+
+// window.onerror = function (err) {
+//   console.log(err);
+// }
+
+// throw new Error('hi ')
